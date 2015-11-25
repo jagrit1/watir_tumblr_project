5e31f07f0a8ed87e71c6eb724b4a4469543ad29c
Academy09 <jgyawali@spartaglobal.com>
2015-11-25 18:11:36 +0000

    latest changes to the watir tumblr project

--- watirscript.rb
+++ watirscript.rb
@@ -1,16 +1,23 @@
 require 'watir'
+require 'yaml'
 browser = Watir::Browser.new :firefox
 browser.goto "https://www.tumblr.com/login"
-browser.text_field(id: "signup_email").set "jaggywali@gmail.com"
-browser.text_field(id: "signup_password").set "spartaglobal"
+
+file = YAML.load_file('./logincredentials.yml')
+  file["user"].each do |yml_user|
+		browser.text_field(id: 'signup_email').set "#{yml_user["email"]}"
+		browser.text_field(id: 'signup_password').set "#{yml_user["password"]}"
+	end
 browser.button(id: "signup_forms_submit").click
 
 browser.i(class: "icon_post_text").click
 browser.element(class: "editor-plaintext").send_keys "Hello World!\t"
 browser.element(class: "editor-richtext").send_keys "This is my text\t #watir"
 browser.element(class: "create_post_button").click
+browser.goto "jagritgyawali.tumblr.com"
+browser.alert.ok
 
-verification = browser.elements(class: "post_body")
+verification = browser.elements(class: "body-text")
 verification.each do |a| 
   if a.text.match /This is my text/
 		puts "#{a.text},\nThe post has been successfully made and listed in dashboard!"
@@ -19,5 +26,3 @@ verification.each do |a|
 		puts "The post could not be found"
   end
 end
-
-browser.close
