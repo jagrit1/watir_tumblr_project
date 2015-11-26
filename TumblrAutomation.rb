30dac9d694a198960731a30d22e30188f8daed93
Academy09 <jgyawali@spartaglobal.com>
2015-11-26 12:09:16 +0000

    updated automation of tumblr logging in and making a post along with verifying the presence of post

---
+++ TumblrAutomation.rb
@@ -0,0 +1,39 @@
+module Tumblr
+def login(email, password, browser)
+  file = YAML.load_file('./logincredentials.yml')
+  browser.text_field(id: 'signup_email').set file["user"][0]["email"]
+  browser.text_field(id: 'signup_password').set file["user"][0]["password"]
+  browser.button(id: "signup_forms_submit").click
+end
+
+def post(browser)
+
+  browser.i(class: "icon_post_text").click
+  browser.element(class: "editor-plaintext").send_keys "Hello World!\t"
+  browser.element(class: "editor-richtext").send_keys "This is my text\t #watir"
+  browser.element(class: "create_post_button").click
+end
+
+def navigate(browser)
+
+  browser.goto "jagritgyawali.tumblr.com"
+  browser.alert.ok
+end
+
+def verification(browser)
+verification = browser.elements(class: "body-text")
+verification.each do |a| 
+  if a.text.match /This is my text/
+		puts "#{a.text},\nThe post has been successfully made and listed in dashboard!"
+		break;
+	else
+		puts "The post could not be found"
+  end
+  end
+
+end
+
+def teardown (browser)
+  browser.close
+end
+end
--- watirscript.rb
+++ watirscript.rb
@@ -1,29 +1,16 @@
 require 'watir'
 require 'yaml'
-browser = Watir::Browser.new :firefox
-browser.goto "https://www.tumblr.com/login"
-
-file = YAML.load_file('./logincredentials.yml')
-  file["user"].each do |yml_user|
-		browser.text_field(id: 'signup_email').set "#{yml_user["email"]}"
-		browser.text_field(id: 'signup_password').set "#{yml_user["password"]}"
-	end
-browser.button(id: "signup_forms_submit").click
-
-browser.i(class: "icon_post_text").click
-browser.element(class: "editor-plaintext").send_keys "Hello World!\t"
-browser.element(class: "editor-richtext").send_keys "This is my text\t #watir"
-browser.element(class: "create_post_button").click
-browser.goto "jagritgyawali.tumblr.com"
-browser.alert.ok
-verification = browser.elements(class: "body-text")
-verification.each do |a| 
-  if a.text.match /This is my text/
-		puts "#{a.text},\nThe post has been successfully made and listed in dashboard!"
-		break;
-	else
-		puts "The post could not be found"
-  end
+$LOAD_PATH<<'.'
+require 'TumblrAutomation'
+class TumblrTest
+  browser = Watir::Browser.new :firefox
+  browser.goto "https://www.tumblr.com/login"
+  file = YAML.load_file('./logincredentials.yml')
+  email = file["email"]
+  password = file["password"]
+  TumblrTest.new.login(email, password, browser)
+  TumblrTest.new.post(browser)
+  TumblrTest.new.navigate(browser)
+  TumblrTest.new.verification(browser)
+  TumblrTest.new.teardown(browser)
 end
-
-browser.close
