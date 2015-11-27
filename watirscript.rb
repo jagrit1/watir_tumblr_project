aad9ddd97260b4e40674b43aefbd0eff770fe633
Academy09 <jgyawali@spartaglobal.com>
2015-11-27 16:48:48 +0000

    modified waterscript.rb file and a slight change on TumblrTest class

--- watirscript.rb
+++ watirscript.rb
@@ -4,15 +4,15 @@ $LOAD_PATH<<'.'
 require 'TumblrAutomation'
 include Tumblr
 class TumblrTest
+  a = TumblrTest.new
   browser = Watir::Browser.new :firefox
   browser.goto "https://www.tumblr.com/login"
   file = YAML.load_file('./logincredentials.yml')
   email = file["email"]
   password = file["password"]
-  TumblrTest.new.login(email, password, browser)
-  TumblrTest.new.post(browser)
-  TumblrTest.new.navigate(browser)
-  TumblrTest.new.verification(browser)
-  TumblrTest.new.deletepost(browser)
-  TumblrTest.new.teardown(browser)
+  a.login(email, password, browser)
+  a.post(browser)
+  a.navigate(browser)
+  a.verification(browser)
+  a.teardown(browser)
 end
